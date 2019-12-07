23caf51880149e06bd974dd3e5969ff131bc98ca
SiddPpatel <55369429+SiddPpatel@users.noreply.github.com>
2019-12-07 14:23:20 -0500

    jh

--- CSC 4710/src/part6_DB.java
+++ CSC 4710/src/part6_DB.java
@@ -1,54 +1,52 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class part6_DB {
 	private static Connection connect = null;
-	
+
 	public static List<String> part6() {
 		List<String> list=new ArrayList<String>();
 		List<String> list1=new ArrayList<String>();
 		try {
-			String qury = "SELECT user_id FROM projectdb.review_item where review_rating = \"Excellent\" GROUP BY user_id HAVING COUNT(user_id) >= 3 ;";
-			String qury1 = "select distinct(user_id) FROM projectdb.review_item;\r\n";
-			
+			String qury = "SELECT item_owner_id FROM projectdb.review_item where review_rating = \"Excellent\" GROUP BY user_id HAVING COUNT(user_id) >= 3 ;";
+			String qury1 = "select distinct(item_owner_id) FROM projectdb.review_item;\r\n";
+
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connect = DriverManager.getConnection("jdbc:mysql://:3306/projectdb?"
-	                + "user=john&password=pass1234");
-			
-        	Statement statement =  (Statement) connect.createStatement();
-        	ResultSet result = statement.executeQuery(qury);
-        	
-        	Statement statement1 =  (Statement) connect.createStatement();
-        	ResultSet result1 = statement1.executeQuery(qury1);
-        	
-        	while(result.next()) {
-        		list.add(result.getString("user_id"));
-        	}
-        	
-        	while(result1.next()) {
-        		list1.add(result1.getString("user_id"));
-        	}
-        	
-        	statement.close();
-        	statement1.close();
+					+ "user=john&password=pass1234");
 
-        	System.out.println(list);
+			Statement statement =  (Statement) connect.createStatement();
+			ResultSet result = statement.executeQuery(qury);
+
+			Statement statement1 =  (Statement) connect.createStatement();
+			ResultSet result1 = statement1.executeQuery(qury1);
+
+			while(result.next()) {
+				list.add(result.getString("item_owner_id"));
+			}
+
+			while(result1.next()) {
+				list1.add(result1.getString("item_owner_id"));
+			}
+
+			statement.close();
+			statement1.close();
+
+			System.out.println(list);
 		}catch (Exception e) {
 			System.out.println(e);
 		}		
-		
+
 		List<String> union = new ArrayList<String>(list1);
 		union.addAll(list);
 		List<String> intersection = new ArrayList<String>(list1);
 		intersection.retainAll(list);
 		union.removeAll(intersection);
-		
+
 		return union;
 	}
 
@@ -80,7 +78,7 @@ public class part6_DB {
 			System.out.println(e);
 		}
 		return userlist;
-		
+
 	}
 
 }
